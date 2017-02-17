c67139e453f18c3d102520a0c7339f38f8878db4
Jo Mantelers <yomomano@gmail.com>
2017-02-17 20:52:19 +0200

    BIM demo (in progress)

---
+++ BIM/demos/ProjectData/Samex.js
@@ -0,0 +1,15 @@
+var projectData = {
+    debug: false,
+    levelid: 'B3',
+    levels: {
+        'B3': {
+            models: [
+                { url: 'HenglungBIM20170207.enumavr', }
+            ],
+            // level specific data mapping
+            datamapping: {
+                '/Home/GetFacilityModels': 'FacilityModels.js'
+            },
+        },
+    },
+};
---
+++ BIM/demos/ProjectData/YX.js
@@ -0,0 +1,10 @@
+﻿var projectData = {
+    debug: false,
+    levels: {
+        '10F': {
+            models: [
+                    { url: 'YX 10F-v2.enumavr', castshadow: true, receiveshadow: true },
+            ],
+        },
+    },
+};
